70135ee496faab52d130f91444cd66bf23dccaff
Aman <99051262+Aman96-star@users.noreply.github.com>
2025-08-08 23:49:34 +0530

    no change

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ return(
         className="logo"
         src="https://brandlogo.org/wp-content/uploads/2024/09/Perplexity-AI-App-Icon-2023.png.webp"
         // src={LOGO_URL}
+        // sdkasdmk
         alt="Logo"
       />
     </div>
