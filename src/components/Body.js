5dc771b3e991d6a7763283d1280adf48ef57de58
Aman <99051262+Aman96-star@users.noreply.github.com>
2025-08-19 17:34:29 +0530

    today i have make a feature of filter a cards by rating but in a console not in ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,85 @@
 import ResturantCard from "./Resturantcard";
 import RestaurantList from "../utils/mockdata";
 
-const Body =()=>{
-    return (
-    <div className="res-container">
-      {RestaurantList.map((res) => (
-        <ResturantCard
-          key={res.id}
-          brand={res.brand}
-          rating={res.rating}
-          describe={res.describe}
-          location={res.location}
-          price={res.price}
-          image={res.image}
-        />
-      ))}
-    </div>
+const Body = () => {
+  let RestaurantList =[
+      {
+        "id": 1,
+        "name": "McDonald's",
+        "rating": 1.6,
+        "deliveryTime": "25-30 mins",
+        "cuisine": ["Burgers", "Beverages", "Cafe"],
+        "location": "Ameerpet metro, L&T Metro Rail",
+        "price": "₹119",
+       
+      },
+        {
+          "id": 2,
+          "name": "KFC",
+          "rating": 1.9,
+          "deliveryTime": "40-45 mins",
+          "cuisine": ["Burgers", "Fast Food", "Rolls"],
+          "location": "Concourse Level",
+          "price": "₹59",
+          
+        },
+        {
+          "id": 3,
+          "name": "Pizza Hut",
+          "rating": 4.1,
+          "deliveryTime": "40-45 mins",
+          "cuisine": ["Pizzas"],
+          "location": "SR Nagar",
+          "price": "₹99",
+          
+        },
+        {
+          "id": 4,
+          "name": "Burger King",
+          "rating": 4.2,
+          "deliveryTime": "30-35 mins",
+          "cuisine": ["Burgers", "American"],
+          "location": "Secunderabad",
+          "price": "₹59",
+          
+        },
+  ];
+  return (
+    <>
+      <div className="body">
+        <div className="filter">
+          {/* here i am using a onclick event with callback function */}
+          <div
+            className="filter-btn"
+            onClick={() => {
+              // console.log("button clicked");
+                RestaurantList =RestaurantList.filter(
+                  (res)=>res.rating>2
+                );
+                console.log("filtered list",RestaurantList);
+            }}
+          >
+            top-rated resturants
+          </div>
+        </div>
+        <div className="res-container">
+          {RestaurantList.map((res) => (
+            <ResturantCard
+
+        
+              key={res.id}
+  name={res.name}                     // ✅ from data
+  rating={res.rating}                 // ✅ from data
+  // cuisine={res.cuisine.join(", ")}    // ✅ convert array → string
+  location={res.location}             // ✅ from data
+  price={res.price}                   // ✅ from data
+  deliveryTime={res.deliveryTime} 
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
-
 
 export default Body;
